a6698ac882cd5ba100a95b16ca1394649675b58a
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2025-10-21 14:28:02 +0700

    fetch data dari tabel biodata

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-let mysql = require('mysql');
+let mysql = require('mysql2');
 const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
 
 app.listen(port, () => {
@@ -19,4 +19,15 @@ const db = mysql.createConnection({
     password: 'Kudangamuk',
     database: 'mahasiswa'
     port: 3309
+});
+
+app.get('/api/mahasiswa', (req, res) => {
+    db.query('SELECT * FROM biodata', (err, results) => {
+        if (err) {
+            console.error('Error executing query:', + err.stack);
+            res.status(500).send('Error fetching users');
+            return;
+        }
+        res.json(results);
+    });
 });
